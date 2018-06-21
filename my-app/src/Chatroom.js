5bafa2dfda3e4c878fd40d14ffe69079de90c377
atanasovanadya <nadi_29@abv.bg>
2018-06-21 08:55:57 +0300

    new files

---
+++ my-app/src/Chatroom.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css'
+
+import Message from './Message.js'
+
+class Chatroom extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      chats: [],
+    }
+
+    this.submitMessage = this.submitMessage.bind(this)
+  }
+
+  componentDidMount() {
+    this.scrollToBot()
+  }
+
+  componentDidUpdate() {
+    this.scrollToBot()
+  }
+
+  scrollToBot() {
+    ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(
+      this.refs.chats
+    ).scrollHeight
+  }
+
+  submitMessage(e) {
+    e.preventDefault()
+
+    this.setState(
+      {
+        chats: this.state.chats.concat([
+          {
+            content: <p>{ReactDOM.findDOMNode(this.refs.msg).value}</p>,
+          },
+        ]),
+      },
+      () => {
+        ReactDOM.findDOMNode(this.refs.msg).value = ''
+      }
+    )
+  }
+
+  render() {
+    const { chats } = this.state
+
+    return (
+      <div className="chatroom">
+        <h3>Eraserhead</h3>
+        <ul className="chats" ref="chats">
+          {chats.map(chat => <Message chat={chat} />)}
+        </ul>
+        <form className="input" onSubmit={e => this.submitMessage(e)}>
+          <input type="text" ref="msg" />
+          <input type="submit" value="SEND" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Chatroom
---
+++ my-app/src/Message.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Message = ({ chat, user }) => <li className="message">{chat.content}</li>
+
+export default Message
